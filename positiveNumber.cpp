278cb5f9819e1cc4fb437a12da851566a9520e21
Shashwat Singh <shashawat15@gmail.com>
2025-08-02 23:07:35 +0530

    Add files via upload

---
+++ positiveNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int val;
+    cout << "Val: ";
+    cin >> val;
+
+    if (val >= 0)
+    {
+        cout << val;
+    }
+
+    return 0;
+}
